599099814d1137d463454309f544e544b83494a7
mingpepe <mingpepemath@gmail.com>
2024-01-30 23:46:52 +0800

    feat: toggle kanji button on NHK easy news

---
+++ src/nhk.js
@@ -0,0 +1,15 @@
+function clickToggleButton() {
+    const button = document.querySelector('.tool-buttons__item.btn.toggle-ruby.js-toggle-ruby.hide-sp');
+    if (button) {
+        button.click();
+    }
+    else {
+        console.log('button not found');
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'c') {
+        clickToggleButton();
+    }
+});
