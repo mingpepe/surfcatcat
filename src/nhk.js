92a2169006abafc069ef43237feda63e6d8b8798
mingpepe <mingpepemath@gmail.com>
2024-02-04 21:06:19 +0800

    feat: add keyboard control for speak rate

--- src/nhk.js
+++ src/nhk.js
@@ -8,13 +8,47 @@ function clickToggleButton() {
     }
 }
 
+var rate = 1;
+
+function readRateControl(key) {
+    switch (key) {
+        case 'q':
+            rate = Math.max(0.1, rate - 0.1);
+            break;
+        case 'w':
+            rate = Math.min(1, rate + 0.1);
+            break;
+        case 'o':
+            rate = Math.max(0.1, rate - 0.5);
+            break;
+        case 'p':
+            rate = Math.min(1, rate + 0.5);
+            break;
+    }
+}
+
 document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'c':
+            if (!event.ctrlKey) {
+                clickToggleButton();
+            }
+            break;
+        case 'q':
+        case 'w':
+        case 'o':
+        case 'p':
+            readRateControl(event.key);
+            break;
+        default:
+            break;
+    }
     if (event.key === 'c' && !event.ctrlKey) {
-        clickToggleButton();
+
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const a = document.getElementById('js-article-body');
     console.log(a);
     var paragraphs = a.querySelectorAll('p');
@@ -22,13 +56,14 @@ document.addEventListener('DOMContentLoaded', function() {
     paragraphs.forEach((p) => {
         const btn = document.createElement('button');
         btn.textContent = 'Read';
-        btn.addEventListener('click', function() {
+        btn.addEventListener('click', function () {
             var textToSpeak = p.textContent.trim();
             var utterance = new SpeechSynthesisUtterance(textToSpeak);
             utterance.lang = 'ja-JP';
+            utterance.rate = rate;
             window.speechSynthesis.cancel();
             window.speechSynthesis.speak(utterance);
         });
         p.parentElement.insertBefore(btn, p);
-    });    
+    });
 });
