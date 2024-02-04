6d79b44c6c3997c12b341f566dea55ea7837b7a8
mingpepe <mingpepemath@gmail.com>
2024-02-04 20:59:48 +0800

    feat: use web speech api to read paragraph on NHK

--- src/nhk.js
+++ src/nhk.js
@@ -13,3 +13,22 @@ document.addEventListener('keydown', (event) => {
         clickToggleButton();
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const a = document.getElementById('js-article-body');
+    console.log(a);
+    var paragraphs = a.querySelectorAll('p');
+    console.log(paragraphs);
+    paragraphs.forEach((p) => {
+        const btn = document.createElement('button');
+        btn.textContent = 'Read';
+        btn.addEventListener('click', function() {
+            var textToSpeak = p.textContent.trim();
+            var utterance = new SpeechSynthesisUtterance(textToSpeak);
+            utterance.lang = 'ja-JP';
+            window.speechSynthesis.cancel();
+            window.speechSynthesis.speak(utterance);
+        });
+        p.parentElement.insertBefore(btn, p);
+    });    
+});
