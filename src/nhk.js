887f66155ff1745956f266fbc2e61f1e9731e000
mingpepe <mingpepemath@gmail.com>
2024-02-17 14:18:10 +0800

    feat: add helper message for NHK

--- src/nhk.js
+++ src/nhk.js
@@ -98,6 +98,58 @@ function extractTextWithoutRt(element) {
     return text;
 }
 
+class HelperPanel {
+    constructor(message) {
+        this._timer = null;
+        this._panel = null;
+
+        const panelWidth = 300;
+        const panelHeight = 200;
+        const panelOpacity = 0.75;
+
+        this._panel = document.createElement('div');
+        this._panel.style.position = 'fixed';
+        this._panel.style.top = '50%';
+        this._panel.style.left = '50%';
+        this._panel.style.transform = 'translate(-50%, -50%)';
+        this._panel.style.width = panelWidth + 'px';
+        this._panel.style.height = panelHeight + 'px';
+        this._panel.style.backgroundColor =
+            'rgba(0, 0, 0, ' + panelOpacity + ')';
+        this._panel.style.color = 'lime';
+        this._panel.style.zIndex = '9999';
+        this._panel.innerHTML = message;
+    }
+
+    show() {
+        if (this._timer === null) {
+            this._timer = setTimeout(() => {
+                document.body.appendChild(this._panel);
+                this._timer = null;
+            }, 300);
+        }
+    }
+
+    hide() {
+        if (this._timer) {
+            clearTimeout(this._timer);
+            this._timer = null;
+        }
+        this._panel.remove();
+    }
+}
+
+const helpMsg = `
+    c: toggle<br>
+    q: rate - 0.1<br>
+    w: rate + 0.1<br>
+    o: rate - 0.5<br>
+    p: rate + 0.5<br>
+    s: copy content to clipboard<br>
+    x: force stop speech<br>
+`;
+const helperPanel = new HelperPanel(helpMsg);
+
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'c':
@@ -116,7 +168,18 @@ document.addEventListener('keydown', (event) => {
             break;
         case 'x':
             window.speechSynthesis.cancel();
+        case 'h':
+            helperPanel.show();
+            break;
         default:
+            break;
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'h':
+            helperPanel.hide();
             break;
     }
 });
