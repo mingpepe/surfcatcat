6d5b9a87d1e5faeb93007cdf4ece805ec4fe8107
mingpepe <mingpepemath@gmail.com>
2024-03-07 09:08:26 +0800

    feat: handle change to another conversation for ChatGPT

--- src/chatgpt.js
+++ src/chatgpt.js
@@ -4,7 +4,7 @@ function init0() {
     if (init1()) {
         main();
     } else {
-        setTimeout(init1, 500);
+        setTimeout(init0, 500);
     }
 }
 
@@ -12,25 +12,38 @@ function init1() {
     var div = document.querySelector('div[data-testid="conversation-turn-2"]');
     if (div) {
         ptr = div.parentElement.childElementCount;
-        console.log(`Set ptr = ${ptr}`);
+        if (ptr % 2 === 0) {
+            ptr++;
+        }
+        console.log(`init1: ptr = ${ptr}`);
         return true;
     }
     return false;
 }
 
-function main() {
-    var div = document.querySelector(
-        `div[data-testid="conversation-turn-${ptr}"]`,
+function getConversationElement(idx) {
+    return document.querySelector(
+        `div[data-testid="conversation-turn-${idx}"]`,
     );
+}
+
+function main() {
+    var div = getConversationElement(ptr);
     if (div) {
         var end = div.querySelectorAll('div.self-end')[0];
         var ready = end.classList.contains('visible');
         if (ready) {
             div.style.display = 'block';
             ptr += 2;
+            console.log(`main: ptr = ${ptr}`);
         } else {
             div.style.display = 'none';
         }
+    } else if (!getConversationElement(ptr - 2)) {
+        // May change to new converation
+        console.log('Change to new converation');
+        init0();
+        return;
     }
     setTimeout(main, 500);
 }
